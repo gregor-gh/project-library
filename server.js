3b68b83df597085e9dc2e0c7196b2bb8f658eb6d
gregor-gh <gregor@live.co.uk>
2021-02-25 19:31:44 +0000

    Changes to try to get Azure working

--- server.js
+++ server.js
@@ -29,22 +29,24 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.static(path.join(__dirname,'client', 'build')));
 
 // redirect to build folder for azure deployment as Azure errors when running "npm install" for create-react-apps
+/* not sure this is working how i expected
 if(process.env.NODE_ENV === 'production') {
   app.get('/*', function (req, res) {
    	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
-}
+} 
+*/
+  //Index page (static HTML)
+  app.route('/')
+    .get(function (req, res) {
+      res.sendFile(process.cwd() + '/views/index.html');
+    });
+
 
 app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
 
 //For FCC testing purposes
 fccTestingRoutes(app);
@@ -60,8 +62,8 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-app.listen(3001, function () {
-  console.log("Listening on port " + 3001);
+app.listen(process.env.PORT, function () {
+  console.log("Listening on port " + process.env.PORT);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
